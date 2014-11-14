737e8aaff207403ed68e33528699781684334b4b
Vladislav Lewin <vlewin@users.noreply.github.com>
2014-11-15 00:07:09 +0100

    add routing for tags values

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :campaigns do
     member do
       match 'search' => 'campaigns#show', via: [:get, :post], as: :search
+      get 'values' => 'campaigns#values', as: :values
       # match 'search' => 'campaigns#search', via: [:get, :post], as: :search
     end
   end
