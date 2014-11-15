c0e94f511687fef29bb1e2fbcc8e6db0af2179bf
Vladislav Lewin <vlewin@users.noreply.github.com>
2014-11-15 11:58:03 +0100

    respect number_of_rows and improve import

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,5 @@
 class Campaign < ActiveRecord::Base
-  has_many :images
+  has_many :images, dependent: :destroy
   has_many :tags, through: :images
 
   def search(values)
@@ -11,13 +11,14 @@ class Campaign < ActiveRecord::Base
     images.joins(:tags).where('tags.name' => tags).uniq.pluck(:value).compact.sort
   end
 
-  def import(file)
+  def import(file, number_of_rows=50)
     spreadsheet = Roo::Excelx.new(file.path, file_warning: :ignore)
     headers = spreadsheet.row(1)
 
     #(2..spreadsheet.last_row).each do |i|
-    (2..5).each do |i|
+    (2..number_of_rows.to_i).each do |i|
       row = Hash[[headers, spreadsheet.row(i)].transpose]
+      next if row['image_url'].blank?
       image = self.images.create(url: row['image_url'])
 
       row.each_with_index do |(name, value), index|
